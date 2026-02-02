a9c338dc112672b826e303f0788054a26861c332
TimVerbeeck <tim.lucario@gmail.com>
2026-02-02 16:00:37 +0100

    added pagination added when searching sets the page number to one

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
@@ -10,18 +12,18 @@ export default function Search({placeholder}: { placeholder: string }) {
     const { replace } = useRouter();
 
 
-    function handleSearch(term: string) {
-        console.log(`Searching... ${term}`);
-
+    const handleSearch = useDebouncedCallback((term) => {
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
         if (term) {
             params.set('query', term);
         } else {
             params.delete('query');
         }
-        replace(`${pathname}?${params.toString()}`);
+        replace(`${pathname}?${params.toString()}`)
+    }, 300);
 
-    }
+
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
