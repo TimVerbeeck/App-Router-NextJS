330c777557e52d920c2f05cf777d79c1fd11b072
TimVerbeeck <tim.lucario@gmail.com>
2026-02-03 15:25:20 +0100

    added server side form validation to edit-form.tsx
    installed nextauth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,14 +74,26 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    // Validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
     try {
         await sql`
@@ -90,8 +102,7 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
     } catch (error) {
-        // We'll also log the error to the console for now
-        console.error(error);
+        // If a database error occurs, return a more specific error.
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
